716411d51b9c8d8956ed04beb0babe9d8118d6f7
Joshua Newman <jonewman1020@gmail.com>
2015-01-15 16:32:08 -0800

    Finished propotype of twittler. Need to add page colour scheme

--- data_generator.js
+++ data_generator.js
@@ -15,10 +15,11 @@ window.users = Object.keys(streams.users);
 
 // utility function for adding tweets to our data structures
 var addTweet = function(newTweet){
-  var username = newTweet.user;
+  var username = newTweet.user; 
   streams.users[username].push(newTweet);
   streams.home.push(newTweet);
 };
+
 
 // utility function
 var randomElement = function(array){
@@ -46,24 +47,27 @@ var generateRandomTweet = function(){
   addTweet(tweet);
 };
 
-for(var i = 0; i < 10; i++){
   generateRandomTweet();
-}
+
 
 var scheduleNextTweet = function(){
   generateRandomTweet();
-  setTimeout(scheduleNextTweet, Math.random() * 1500);
+  setTimeout(scheduleNextTweet, Math.random() * 1000);
 };
+
 scheduleNextTweet();
+
 
 // utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
-var writeTweet = function(message){
+var writeTweet = function(message, time){
   if(!visitor){
     throw new Error('set the global visitor property!');
   }
   var tweet = {};
   tweet.user = visitor;
+  streams.users[visitor] = [];
   tweet.message = message;
+  tweet.created_at = time;
   addTweet(tweet);
 };
